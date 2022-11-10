779cf2e143c3872d6ffc988620bd646edf2b79b0
Swatij1103 <110924144+Swatij1103@users.noreply.github.com>
2022-11-10 12:17:00 +0530

    Update Exp1.py

--- Exp1.py
+++ Exp1.py
@@ -1,6 +1,6 @@
 charachter  = list(map(chr,range(ord('a'),ord('z')+1)))+list(map(chr,range(ord('A'),ord('Z')+1)))
 
-def encode(text,key=2):
+def encode(text,key):
     cipher_text =""
     for x in text:
         if x == " ":
@@ -10,7 +10,7 @@ def encode(text,key=2):
             cipher_text+=charachter[(pos+key)%52]
     return cipher_text
 
-def decode(cipher,key=2):
+def decode(cipher,key):
     plain_text = ""
     for ciph in cipher:
         if ciph == "$":
@@ -31,12 +31,13 @@ def calfrequency(text):
             frequency[x]+=offset
         else:
             frequency[x]=offset
-    return frequency  
+    return frequency
 
 plain_text = input("Enter the Text to encode : ")
-cipher = encode(plain_text,key=2)
+key =int(input("Enter key : "))
+cipher = encode(plain_text,key)
 print(cipher)
-text = decode(cipher,key=2)
+text = decode(cipher,key)
 print(text)
 text = calfrequency(text)
 print(text)
